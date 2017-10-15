89406bc5279e3e6f71da526e7865d6bea4784d4c
johnalexandergreene <john@fleen.org>
2017-10-15 01:43:13 -0500

    working on forsythiaSpinnerLoopingFramesGenerator

--- forsythiaSpinnerLoopingFramesGenerator/ForsythiaSpinnerLoopingFramesGenerator.java
+++ forsythiaSpinnerLoopingFramesGenerator/ForsythiaSpinnerLoopingFramesGenerator.java
@@ -1,5 +1,11 @@
 package org.fleen.bread.forsythiaSpinnerLoopingFramesGenerator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+import org.fleen.forsythia.core.grammar.ForsythiaGrammar;
+
 /*
  * Given
  *   a viewport definition (height and width) 
@@ -25,8 +31,109 @@ package org.fleen.bread.forsythiaSpinnerLoopingFramesGenerator;
  */
 public class ForsythiaSpinnerLoopingFramesGenerator{
   
-  public void generate(int w,int h,int dir,String grammarpath,String exportpath){
+  public void generate(int w,int h,int d,String grammarpath,String exportpath){
+    viewportwidth=w;
+    viewportheight=h;
+    flowdir=d;
+    initGrammar(grammarpath);
+    initExport(exportpath);
+    createFrames();}
+  
+  /*
+   * ################################
+   * VIEWPORT DEFINITION
+   * ################################
+   */
+  
+  int 
+    viewportwidth,
+    viewportheight;
+  
+  /*
+   * ################################
+   * FLOW DIRECTION
+   * ################################
+   */
+  
+  public static final int 
+    FLOWDIR_NORTH=0,
+    FLOWDIR_EAST=1,
+    FLOWDIR_SOUTH=2,
+    FLOWDIR_WEST=3;
+  
+  int flowdir;
+  
+  /*
+   * ################################
+   * GRAMMAR
+   * ################################
+   */
+  
+  ForsythiaGrammar grammar=null;
+  
+  private void initGrammar(String path){
+    grammar=null;
+    try{
+      File f=new File(path);
+      grammar=importGrammarFromFile(f);
+    }catch(Exception x){
+      System.out.println("exception in grammar import");
+      x.printStackTrace();}}
+    
+  private ForsythiaGrammar importGrammarFromFile(File file){
+    FileInputStream fis;
+    ObjectInputStream ois;
+    ForsythiaGrammar g=null;
+    try{
+      fis=new FileInputStream(file);
+      ois=new ObjectInputStream(fis);
+      g=(ForsythiaGrammar)ois.readObject();
+      ois.close();
+    }catch(Exception x){}
+    return g;}
+  
+  /*
+   * ################################
+   * FRAME EXPORT
+   * ################################
+   */
+  
+  File exportdir;
+  
+  private void initExport(String path){
+    exportdir=new File(path);}
+  
+  /*
+   * ################################
+   * CREATE FRAMES
+   * ################################
+   */
+  
+  /*
+   * gather the rectangular metagons from the grammar
+   * 
+   * create a strip of rectangles to cover the viewport.
+   * This is the startend.
+   * 
+   * cultivate the rectangles. Render to viewport (and to a frame, and export that frame)
+   * move the viewport across the startend, 1 pixel row at a time
+   * when we hit the edge create a new adjoining rectangle
+   * keep moving.
+   * when the viewport leaves a rectangle, discard the rectangle.
+   * (but don't discard the startend)
+   * keep adding rectangles and reaversing them until the sum of the rectangles (including the startend) meets our desired loop length.
+   * After that we move back to the startend
+   * keep moving until we arrive back at out start position.
+   * then we're done.
+   */
+  private void createFrames(){
+    
+    
     
   }
+  
+  
+  
+  
 
 }
